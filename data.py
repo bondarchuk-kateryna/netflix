0cb1b66d7dc1eb5e3b7bdce816084ed2e066c9f3
kateryna.bondarchuk <bondarchuk@kse.org.ua>
2023-01-27 10:01:16 +0200

    task 3

--- data.py
+++ data.py
@@ -18,3 +18,29 @@ def build_age_categories() -> None:
     ax.pie(df2, labels=labels, autopct="%1.1f%%", shadow=True)
     ax.set_title("Percentage of movies by age categories")
     pyplt.show()
+
+def build_histogram() -> None:
+    top1000 = extract_top_titles(types=["MOVIE", "SHOW"])
+
+    df = top1000[top1000["genres"] != "[]"]
+
+    df = pandas.DataFrame(df)
+    df["genres"] = df["genres"].apply(ast.literal_eval)
+
+    genres_counts = (
+        df.explode(column="genres").groupby(["genres"]).size().reset_index(name="Count")
+    )
+
+    genres = list(genres_counts["genres"])
+    counts = list(genres_counts["Count"])
+
+    pyplt.bar(genres, counts)
+    pyplt.xticks(rotation=90)
+    pyplt.xlabel("Genre")
+    pyplt.ylabel("Count of movies/shows")
+
+    pyplt.title("Count by genre")
+
+    pyplt.show()
+
+
